52fb48d47fb01ba3f07e9db5fdb6dc78d2098545
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2024-05-14 02:44:19 +0200

    Fixed casting

--- sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPConfigTest.java
+++ sbnz-integracija-projekta/service/src/test/java/com/ftn/sbnz/service/tests/CEPConfigTest.java
@@ -11,6 +11,7 @@ import org.kie.api.runtime.KieSession;
 import com.ftn.sbnz.model.models.Breakdown;
 import com.ftn.sbnz.model.models.ElectricCar;
 import com.ftn.sbnz.model.models.FaultCodes;
+import com.ftn.sbnz.model.models.GasCar;
 import com.ftn.sbnz.model.models.Symptom;
 import com.ftn.sbnz.model.models.Util;
 import com.ftn.sbnz.model.models.FaultProblems.FaultProblem;
@@ -19,56 +20,62 @@ import com.ftn.sbnz.model.models.FaultProblems.FaultProblemKinds;
 
 public class CEPConfigTest {
 
-    // @Test
-    // public void test() {
-    //     // KieServices ks = KieServices.Factory.get();
-    //     // KieContainer kContainer = ks.getKieClasspathContainer(); 
-    //     // KieSession ksession = kContainer.newKieSession("cepKsession");
-    //     KieServices ks = KieServices.Factory.get();
-    //     KieContainer kContainer = ks.getKieClasspathContainer();
-    //     KieSession ksession = kContainer.newKieSession("carKsession");
-    //     List<Symptom> s = new ArrayList<>();
-    //     s.add(Symptom.HEADLIGHTS_ON);
-    //     s.add(Symptom.NO_START);
-    //     Breakdown b = new Breakdown("kvar1", s);
-    //     b.setCar(new GasCar());
-    //     ksession.insert(b);
-    //     int ruleCount = ksession.fireAllRules();
-    // }
+    @Test
+    public void test() {
+        // KieServices ks = KieServices.Factory.get();
+        // KieContainer kContainer = ks.getKieClasspathContainer(); 
+        // KieSession ksession = kContainer.newKieSession("cepKsession");
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession ksession = kContainer.newKieSession("carKsession");
+        List<Symptom> s = new ArrayList<>();
+        s.add(Symptom.HEADLIGHTS_ON);
+        s.add(Symptom.NO_START);
+        Breakdown b = new Breakdown("kvar1", s);
+        GasCar car = new GasCar();
+         ksession.insert(car);
+        b.setCar(car);
+        ksession.insert(b);
+        int ruleCount = ksession.fireAllRules();
+    }
 
-    //     @Test
-    // public void tes1() {
-    //     // KieServices ks = KieServices.Factory.get();
-    //     // KieContainer kContainer = ks.getKieClasspathContainer(); 
-    //     // KieSession ksession = kContainer.newKieSession("cepKsession");
-    //     KieServices ks = KieServices.Factory.get();
-    //     KieContainer kContainer = ks.getKieClasspathContainer();
-    //     KieSession ksession = kContainer.newKieSession("carKsession");
-    //     List<Symptom> s = new ArrayList<>();
-    //     s.add(Symptom.DIFFICULT_STEERING);
-    //     s.add(Symptom.FLAT_TIRE);
-    //     Breakdown b = new Breakdown("kvar2", s);
-    //     b.setCar(new ElectricCar());
-    //     ksession.insert(b);
-    //     int ruleCount = ksession.fireAllRules();
-    // }
+        @Test
+    public void tes1() {
+        // KieServices ks = KieServices.Factory.get();
+        // KieContainer kContainer = ks.getKieClasspathContainer(); 
+        // KieSession ksession = kContainer.newKieSession("cepKsession");
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession ksession = kContainer.newKieSession("carKsession");
+        List<Symptom> s = new ArrayList<>();
+        s.add(Symptom.DIFFICULT_STEERING);
+        s.add(Symptom.FLAT_TIRE);
+        Breakdown b = new Breakdown("kvar2", s);
+         ElectricCar car = new ElectricCar();
+         ksession.insert(car);
+        b.setCar(car);
+        ksession.insert(b);
+        int ruleCount = ksession.fireAllRules();
+    }
 
-    //      @Test
-    // public void test3() {
-    //     // KieServices ks = KieServices.Factory.get();
-    //     // KieContainer kContainer = ks.getKieClasspathContainer(); 
-    //     // KieSession ksession = kContainer.newKieSession("cepKsession");
-    //     KieServices ks = KieServices.Factory.get();
-    //     KieContainer kContainer = ks.getKieClasspathContainer();
-    //     KieSession ksession = kContainer.newKieSession("carKsession");
-    //     List<Symptom> s = new ArrayList<>();
-    //     // s.add(Symptom.JERKING);
-    //     // s.add(Symptom.FLAT_TIRE);
-    //     Breakdown b = new Breakdown("kvar2", s);
-    //     b.setCar(new ElectricCar());
-    //     ksession.insert(b);
-    //     int ruleCount = ksession.fireAllRules();
-    // }
+         @Test
+    public void test3() {
+        // KieServices ks = KieServices.Factory.get();
+        // KieContainer kContainer = ks.getKieClasspathContainer(); 
+        // KieSession ksession = kContainer.newKieSession("cepKsession");
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession ksession = kContainer.newKieSession("carKsession");
+        List<Symptom> s = new ArrayList<>();
+        // s.add(Symptom.JERKING);
+        // s.add(Symptom.FLAT_TIRE);
+        Breakdown b = new Breakdown("kvar2", s);
+        ElectricCar car = new ElectricCar();
+         ksession.insert(car);
+        b.setCar(car);
+        ksession.insert(b);
+        int ruleCount = ksession.fireAllRules();
+    }
 
 
         @Test
@@ -85,12 +92,15 @@ public class CEPConfigTest {
             // s.add(Symptom.FLAT_TIRE);
             Breakdown b = new Breakdown("kvar2", s);
             ElectricCar car = new ElectricCar();
+       
+            b.setCar(car);
             car.setFaultCheck(true);
             FaultCodes code = Util.randomCode(); 
             code = FaultCodes.ELECTRICAL_CODE_C;
             car.addCode(code);
             b.setCar(car);
             FaultProblem problem = new FaultProblem();
+              ksession.insert(car);
             ksession.insert(b);
             ksession.insert(problem);
             ksession.getAgenda().getAgendaGroup("checking faults").setFocus();
