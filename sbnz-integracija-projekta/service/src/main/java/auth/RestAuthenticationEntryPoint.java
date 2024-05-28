c0444fc3b4f8c65fb2893151dc1b827666f8782e
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2024-05-28 18:48:43 +0200

    added authentication clasees as well as login endpoint

---
+++ sbnz-integracija-projekta/service/src/main/java/auth/RestAuthenticationEntryPoint.java
@@ -0,0 +1,59 @@
+package auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@ControllerAdvice
+public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	@Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+    	System.out.println(authException);
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
+
+    @ExceptionHandler (value = {AccessDeniedException.class})
+    public void commence(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        // 403
+        setResponseError(response, HttpServletResponse.SC_FORBIDDEN, String.format("Access Denies: %s", accessDeniedException.getMessage()));
+    }
+
+
+
+	@ExceptionHandler (value = {MethodArgumentNotValidException.class})
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+		List<ObjectError> errorList = ex.getBindingResult().getAllErrors();
+        StringBuilder sb = new StringBuilder("Request finished with validation errors: \n");
+
+        for (ObjectError error : errorList ) {
+        	sb.append("Field ");
+            FieldError fe = (FieldError) error;
+            sb.append(fe.getField() + " ");
+            sb.append(error.getDefaultMessage()+ "!\n\n");
+        }
+
+        return new ResponseEntity<>(sb.toString(), HttpStatus.BAD_REQUEST);
+	}
+
+    private void setResponseError(HttpServletResponse response, int errorCode, String errorMessage) throws IOException{
+        response.setStatus(errorCode);
+        response.getWriter().write(errorMessage);
+        response.getWriter().flush();
+        response.getWriter().close();
+    }
+}
