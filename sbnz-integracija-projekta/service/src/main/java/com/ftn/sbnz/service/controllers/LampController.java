09a2095a419cfd4f4e29f22974a0a43f10354805
Srđan Stjepanović <srdjan.stjepanovic01@gmail.com>
2024-06-05 23:22:42 +0200

    finished backend for getting repairment based on lamps using backward

--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/LampController.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/controllers/LampController.java
@@ -1,11 +1,27 @@
 package com.ftn.sbnz.service.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.ftn.sbnz.model.models.Lamp;
+import com.ftn.sbnz.service.dtos.repairment.RepairmentDTO;
+import com.ftn.sbnz.service.services.interfaces.ILampService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.security.PermitAll;
+import java.util.List;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/lamp")
 public class LampController {
+
+    @Autowired
+    ILampService lampService;
+
+    @PermitAll
+    @PostMapping("/lamps/check/{carId}")
+    public ResponseEntity<?> create(@RequestBody List<Lamp> lamps, @PathVariable Integer carId) {
+        List<RepairmentDTO> repairmentDTOs = lampService.getRepairmentByLamps(lamps, carId);
+        return ResponseEntity.ok(repairmentDTOs);
+    }
 }
--- sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/LampService.java
+++ sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/LampService.java
@@ -1,8 +1,56 @@
 package com.ftn.sbnz.service.services;
 
+import com.ftn.sbnz.model.models.Car;
+import com.ftn.sbnz.model.models.Lamp;
+import com.ftn.sbnz.model.models.Repairment;
+import com.ftn.sbnz.service.dtos.repairment.RepairmentDTO;
+import com.ftn.sbnz.service.repositories.ICarRepository;
 import com.ftn.sbnz.service.services.interfaces.ILampService;
+import com.ftn.util.Util;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.kie.api.runtime.KieSession;
+
+import java.util.List;
 
 @Service
 public class LampService implements ILampService {
+
+    @Autowired
+    ICarRepository allCars;
+
+    @Override
+    public List<RepairmentDTO> getRepairmentByLamps(List<Lamp> lamps, Integer carId) {
+        KieServices ks = KieServices.Factory.get();
+        KieContainer kContainer = ks.getKieClasspathContainer();
+        KieSession ksession = kContainer.newKieSession("bwKsession");
+
+        Car car = allCars.findById(carId).get();
+
+        for (Lamp lamp: lamps) {
+            lamp.setPlate(car.getPlate());
+            ksession.insert(lamp);
+        }
+        ksession.insert(car);
+
+        List<Repairment> previous = ksession.getObjects().stream()
+        .filter(r -> r instanceof Repairment)
+        .map(r -> (Repairment) r)
+        .toList();
+
+        int ruleCount = ksession.fireAllRules();
+        System.out.println(ruleCount);
+
+        ksession.halt();
+         List<Repairment> after = ksession.getObjects().stream()
+        .filter(r -> r instanceof Repairment)
+        .map(r -> (Repairment) r)
+        .toList();
+
+        // after - previous
+        return Util.getListDiff(after, previous).stream().map(r -> new RepairmentDTO(r)).toList();
+
+    }
 }
