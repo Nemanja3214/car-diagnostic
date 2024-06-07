d553e4ab965834d75d57ab0429716ab2ec50d8e2
nemanja3214 <nemanja.majstorovic3214@gmail.com>
2024-06-07 16:34:26 +0200

    Added OBD to front and isElectric to back

--- sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/FaultCodes.java
+++ sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/models/FaultCodes.java
@@ -1,8 +1,31 @@
 package com.ftn.sbnz.model.models;
 
 public enum FaultCodes {
-    ELECTRICAL_CODE_A,
-    ELECTRICAL_CODE_B,
-    ELECTRICAL_CODE_C,
+    ELECTRICAL_CODE_A("ELectrical code A", true),
+    ELECTRICAL_CODE_B("ELectrical code B", true),
+    ELECTRICAL_CODE_C("ELectrical code C", true);
+    
+    private final String stringValue;
+    private final boolean show;
 
+    FaultCodes(String stringValue, boolean show) {
+        this.stringValue = stringValue;
+        this.show = show;
+    }
+  public boolean isShow(){
+        return show;
+    }
+
+    public static FaultCodes fromString(String text) {
+        for (FaultCodes symptom : FaultCodes.values()) {
+            if (symptom.stringValue.equalsIgnoreCase(text)) {
+                return symptom;
+            }
+        }
+        return null; // Or throw an exception if the string doesn't match any enum constant
+    }
+
+     public String getStringValue() {
+        return stringValue;
+    }
 }
